aca211b13fda2cb75e46e7cf6513bb526a7ba66d
Gustavo Contreras <guta.contre@hotmail.com>
2025-03-13 20:57:23 -0500

    chore: deploy front

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Configuración para el desarrollo local
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
+  // Configuración para el preview (despliegue de producción)
+  preview: {
+    allowedHosts: ['localhost', 'charismatic-vibrancy-production.up.railway.app'],
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173
+  }
 })
