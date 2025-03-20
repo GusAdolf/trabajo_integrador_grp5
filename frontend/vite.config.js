cb0ca1a9e1315ed17d4740565923237ac515aeb3
Gustavo Contreras <guta.contre@hotmail.com>
2025-03-19 20:48:41 -0500

    fix: add enviroment variables

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Usa la variable de entorno para el dominio del frontend, con un fallback
+const frontDomain = process.env.VITE_FRONT_DOMAIN || 'frontend-production-bdd1.up.railway.app'
+
 export default defineConfig({
   plugins: [react()],
-  // Configuración para el desarrollo local
+  // Configuración para desarrollo local
   server: {
     host: '0.0.0.0',
     port: 5173,
   },
-  // Configuración para el preview (despliegue de producción)
+  // Configuración para el preview (producción)
   preview: {
-    allowedHosts: ['localhost', 'charismatic-vibrancy-production.up.railway.app'],
+    allowedHosts: ['localhost', frontDomain],
     host: '0.0.0.0',
     port: process.env.PORT ? parseInt(process.env.PORT) : 5173
   }
