98e90ec5490eded91c79b77bea84a880984e89e0
Gustavo Contreras <guta.contre@hotmail.com>
2025-03-20 23:27:58 -0500

    fix: backend domain

--- frontend/src/services/citiesService.js
+++ frontend/src/services/citiesService.js
@@ -1,6 +1,6 @@
 export const getCities = async () => {
   try {
-    const response = await fetch(import.meta.env.VITE_APP_API_URL + "/cities");
+    const response = await fetch(import.meta.env.VITE_BACKEND_URL + "/cities");
     if (!response.ok) {
       throw new Error("Error al obtener las ciudades");
     }
