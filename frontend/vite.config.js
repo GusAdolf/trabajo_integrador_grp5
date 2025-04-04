28334e33cbe6c3f91b8cc0f8156fce7a4fc532ff
Gustavo Contreras <guta.contre@hotmail.com>
2025-04-03 19:07:23 -0500

    feat: add  ci-cd to main

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Usa la variable de entorno para el dominio del frontend, con un fallback
+const frontDomain = process.env.VITE_FRONT_DOMAIN || 'frontend-production-bdd1.up.railway.app'
+
 export default defineConfig({
   plugins: [react()],
+  // Configuración para desarrollo local
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
+  // Configuración para el preview (producción)
+  preview: {
+    allowedHosts: ['localhost', frontDomain],
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173
+  }
 })
